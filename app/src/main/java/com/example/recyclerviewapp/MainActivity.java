0fba32f8a259b4d66f68508379f904fc406e987a
harsh gandhi <harshgandhi043@gmail.com>
2022-03-05 13:11:16 +0530

    made new activity for multiple selection recycler view

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.recyclerviewapp.card_rec_view.CardRecViewActivity;
+import com.example.recyclerviewapp.multi_selection_rec_view.MultiSelectionActivity;
 import com.example.recyclerviewapp.normal_rec_view.NormalRecViewActivity;
 import com.example.recyclerviewapp.single_selection_rec_view.SingleSelectionRecViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnNormalRecView,btnCardRecView,btnSingleSelection;
+    private Button btnNormalRecView,btnCardRecView,btnSingleSelection,btnMultipleSelection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
         btnNormalRecView.setOnClickListener(view -> openNormalRecView());
         btnCardRecView.setOnClickListener(view -> openCardRecView());
         btnSingleSelection.setOnClickListener(view -> openSingleSelectionRecView());
+        btnMultipleSelection.setOnClickListener(view -> openMultipleSelectionRecView());
     }
 
     private void initViews(){
         btnNormalRecView = findViewById(R.id.btnNormalRecView);
         btnCardRecView = findViewById(R.id.btnCardRecView);
         btnSingleSelection = findViewById(R.id.btnSingleSelection);
+        btnMultipleSelection = findViewById(R.id.btnMultipleSelection);
     }
 
     private void openNormalRecView(){
@@ -41,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void openSingleSelectionRecView(){
         Intent intent = new Intent(this, SingleSelectionRecViewActivity.class);
+        startActivity(intent);
+    }
+
+    private void openMultipleSelectionRecView(){
+        Intent intent = new Intent(this, MultiSelectionActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/recyclerviewapp/multi_selection_rec_view/MultiSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.recyclerviewapp.multi_selection_rec_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recyclerviewapp.R;
+
+public class MultiSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_selection);
+    }
+}
